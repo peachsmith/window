d17497aef64e62c64ac06bb5a6132d30488fcac9
John Powell <test@example.com>
2022-11-14 15:17:16 -0600

    implemented common dialog renderer for dialog entities

--- demo/dialog/dialog.h
+++ demo/dialog/dialog.h
@@ -6,9 +6,21 @@
 #define DEMO_MAX_DIALOGS 10
 #define DEMO_DIALOG_BUFSIZE 256
 
+// common dialog text rendering speed
+#define DEMO_DIALOG_SPEED_SCALE 4
+
+// common dimensions of a dialog panel
+#define DEMO_DIALOG_WIDTH 224
+#define DEMO_DIALOG_HEIGHT 48
+
 void demo_init_dialogs(eg_app *);
 
+// Deprecated
+// TODO: remove this once demo_common_dialog_renderer is fully implemented
 void demo_render_dialog(eg_app *, eg_dialog *);
+
+// new common dialog renderer
+void demo_common_dialog_renderer(eg_app *, eg_entity *);
 
 void demo_init_info_dialog(eg_app *);
 void demo_open_info_dialog(eg_app *);
