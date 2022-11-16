659638165c5af3c632c4eb921b5309b63d4716bf
John Powell <test@example.com>
2022-11-16 16:28:57 -0600

    converted input actuation menu to an entity

--- demo/scenes/scene0.c
+++ demo/scenes/scene0.c
@@ -9,6 +9,7 @@
 #include "demo/entities/fish_menu.h"
 #include "demo/entities/info_menu.h"
 #include "demo/entities/debug_menu.h"
+#include "demo/entities/input_menu.h"
 #include "demo/entities/info_dialog.h"
 #include "demo/entities/demo_dialog.h"
 #include "demo/entities/jimbo_dialog.h"
@@ -34,6 +35,7 @@ void load_scene_0(eg_app *app)
     eg_add_entity(app, fish_menu_demo_create());
     eg_add_entity(app, info_menu_demo_create());
     eg_add_entity(app, debug_menu_demo_create());
+    eg_add_entity(app, input_menu_demo_create());
 
     // dialogs
     eg_add_entity(app, demo_dialog_demo_create());
