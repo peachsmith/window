b44257dbb1316657e305f874eb5883bf2d1467c3
John Powell <>
2023-12-03 11:16:31 -0600

    removed old crumbs source code since it has been migrated to a different repo

--- demo/assets.h
+++
@@ -1,8 +0,0 @@
-#include "crumbs.h"
-
-#ifndef DEMO_ASSETS_H
-#define DEMO_ASSETS_H
-
-int demo_load_all_assets(cr_app *app);
-
-#endif
--- tootnsploot/assets.h
+++
@@ -1,8 +0,0 @@
-#include "crumbs.h"
-
-#ifndef TNS_ASSETS_H
-#define TNS_ASSETS_H
-
-int tns_load_all_assets(cr_app*);
-
-#endif
